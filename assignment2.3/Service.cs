3e2ae9ea46253dc25db265ddace842543116783d
Alina <a.y.kositsyna@gmail.com>
2023-04-07 13:31:02 +0700

    created classes Teacher and Service

---
+++ assignment2.3/Service.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2._3
+{
+    internal class Service
+    {
+        public string ServiceName { get; set; }
+    }
+}
---
+++ assignment2.3/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2._3
+{
+    internal class Teacher
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Institute { get; set; }
+        public Service PrefferedService;
+
+        public Teacher()
+        {
+            PrefferedService = new Service();
+        }
+
+    }
+}
