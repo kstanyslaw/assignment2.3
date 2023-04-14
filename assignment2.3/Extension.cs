459b289253073d08f4db33b2dbc8a2eb9cb8c4bd
Alina <a.y.kositsyna@gmail.com>
2023-04-14 13:23:02 +0700

    feat(Teacher): created Fio feat(Extension): created class, made method FioExtension created class System

---
+++ assignment2.3/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2._3
+{
+    internal static class Extension
+    {
+        public static string FioExtension(this string input)
+        {
+            string[] fullFio = input.Split(' ', '.');
+            return fullFio[0] + fullFio[1][0] + fullFio[2][0];
+        }
+    }
+}
--- assignment2.3/Teacher.cs
+++ assignment2.3/Teacher.cs
@@ -8,6 +8,9 @@ namespace assignment2._3
 {
     internal class Teacher
     {
+
+        public string Fio => LastName + " " + FirstName + " " + MiddleName;
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string MiddleName { get; set; }
